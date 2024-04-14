45b97e2cd5808bdc42e3d62e555ab7687d1b9087
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-14 20:33:45 +0800

    java

--- ProducerService-8081/src/main/java/com/example/demo/ProducerController.java
+++ ProducerService-8081/src/main/java/com/example/demo/ProducerController.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,8 @@ public class ProducerController {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 	
-	@PostMapping("/send")
-	public String send(@RequestBody String message) {
+	@GetMapping("/send")
+	public String send(@RequestParam String message) {
 		String exchangeName = "MyExchange";
 		rabbitTemplate.convertAndSend(exchangeName, null, message);
 		return "已發送: " + message + " 到 " + exchangeName;
