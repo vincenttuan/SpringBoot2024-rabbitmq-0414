b50887f68b76f71355e460fd36cd2f4356614700
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-14 20:18:32 +0800

    java

---
+++ ConsumerService-8082/src/main/java/com/example/demo/ConsumerService8082Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerService8082Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerService8082Application.class, args);
+	}
+
+}
