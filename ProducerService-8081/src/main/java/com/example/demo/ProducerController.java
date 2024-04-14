eb7ff4a19ed478484b3ca33c156bc090cd880a16
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-14 20:14:50 +0800

    java

--- ProducerService-8081/src/main/java/com/example/demo/ProducerController.java
+++ ProducerService-8081/src/main/java/com/example/demo/ProducerController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.annotation.PostConstruct;
-
 @RestController
 @RequestMapping("/api")
 public class ProducerController {
