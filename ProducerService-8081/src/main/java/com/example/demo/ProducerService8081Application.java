f84f6b2a59b89d7a59e00caf435aff2c17f66552
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-14 20:04:22 +0800

    java

---
+++ ProducerService-8081/src/main/java/com/example/demo/ProducerService8081Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerService8081Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerService8081Application.class, args);
+	}
+
+}
